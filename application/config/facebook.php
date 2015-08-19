a25ce4b7902a2795cc421362e39719651f62ecb9
Bismo Baruno <bismobaruno@gmail.com>
2015-08-19 10:03:07 +0700

    Update facebook.php

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['appId']   = '1591200021136215';
-$config['secret']  = '63ab69f30392fb28b533357aac75976f';
+$config['appId']   = '';
+$config['secret']  = '';
 
 ?>
